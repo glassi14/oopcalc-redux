8926e5b312e4d2ac6ca20d138f2398280bc52f82
glassi14 <dallasmister14@icloud.com>
2024-07-30 21:52:54 -0400

    Changed the help menu to be more detailed.

--- main.cpp
+++ main.cpp
@@ -3,10 +3,13 @@
 #include <iostream>
 
 static void helpMenu() {
-    std::cout << "\nThis calculator can take in any positive number and perform powers (^), multiplication (*), division (/), modulo (%), addition (+) and subtraction (-).\n" << std::endl; 
-    std::cout << "It is compatible with parenthesis but does not understand that two parenthesis together is multiplication.\n\n" << std::endl;
-    std::cout << "An example problem: (8+8)*8*8/8^8\n\n" << std::endl;
-    std::cout << "The program will ignore any input that isn't a number or valid operation.\n\n" << std::endl;
+    std::cout << "\nThis calculator can take in any positive number and perform exponents (^), multiplication (*), division (/), modulo (%), addition (+) and subtraction (-).\n" << std::endl; 
+    std::cout << "It is compatible with parenthesis but does not understand that two parenthesis together is multiplication.\n" << std::endl;
+    std::cout << "It can take floats as in input using (.) but floats will be converted into ints for the final value.\n\n" << std::endl;
+    std::cout << "Valid arguments are: '-help'/'--help' (Brings this menu), '-i' (Prints infix of input), and '-p' (Prints postfix of input).\n" << std::endl;
+    std::cout << "Input the expression argument surrounded by quotes to avoid any terminal confusion.\n\n" << std::endl;
+    std::cout << "An example input: ./programExecutable '(8+8)*8*8/8^8' -p\n\n" << std::endl;
+    std::cout << "The program will ignore any input that isn't a number or valid operation, such as spaces and letters.\n\n" << std::endl;
 }
 
 int main(int argc, char** argv)
